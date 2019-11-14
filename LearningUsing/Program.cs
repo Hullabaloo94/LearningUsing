4a1c9952c547473e1865bea0abac240e35c60e3a
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:51:52 +0000

    initial commit

---
+++ LearningUsing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LearningUsing
+{
+
+    class MyResource : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing our resource.");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var resource = new MyResource()) 
+            {
+                Console.WriteLine("Using my resource"); 
+            }
+            Console.WriteLine("Finished");
+        }
+    }
+}
